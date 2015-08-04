f39f997ce4331f4a5473da8bf58463170f9d509e
colmfeely <colm.feely@gmail.com>
2015-08-04 23:59:04 +0100

    Update MotorisedBike.java

--- MotorisedBike.java
+++ MotorisedBike.java
@@ -3,9 +3,9 @@ public class MotorisedBike extends Bicycle {
 	
 	private boolean requiresLicence;
 
-	public MotorisedBike(String colour,int frameSize,int wheelSize,String frameComposition,Database database) {
+	public MotorisedBike(String model,String colour,int frameSize,int wheelSize,String frameComposition,Database database) {
 
-		super(colour, frameSize, wheelSize, frameComposition,database);
+		super(model,colour, frameSize, wheelSize, frameComposition,database);
 	}
 
 	public boolean isRequiresLicence() {
