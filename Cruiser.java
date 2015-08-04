edaf6eb07f4f1a6c881991d27eb6e8924253b737
colmfeely <colm.feely@gmail.com>
2015-08-04 21:23:19 +0100

    Update Cruiser.java

--- Cruiser.java
+++ Cruiser.java
@@ -3,7 +3,7 @@ public class Cruiser extends Bicycle {
 
 	private boolean hasTassles;
 	
-	public Cruiser() {
+	public Cruiser(String colour,int frameSize,int wheelSize,String frameComposition) {
 		
 		super(colour, frameSize, wheelSize, frameComposition);
 
